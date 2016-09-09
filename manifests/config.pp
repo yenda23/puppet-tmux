71a0bf4a0adb5403eb5187d961877df90c0e4995
yenda23 <john@syncope.cz>
2016-09-09 16:35:07 +0200

    minor changes to fits our ENV

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,18 +1,23 @@
 define tmux::config(
-  $source = 'normal',
-  $target = '/root/.tmux.conf',
-  $owner  = 'root',
-  $group  = '0'
+	$source = 'normal',
+	$target = '/root/.tmux.conf',
+	$owner  = 'root',
+	$group  = 'root'
 ){
-  require ::tmux
-  file{$name:
-    path => $target,
-    source => $source ? {
-      'normal' => [ "puppet:///modules/site_tmux/${::fqdn}/${name}",
-                    "puppet:///modules/site_tmux/${name}",
-                    "puppet:///modules/tmux/normal" ],
-      default => $source
-    },
-    owner => $owner, group => $group, mode => 0600;
-  }
+	require ::tmux
+	file{ $name:
+		path => $target,
+		source => $source ? {
+			'normal' => [
+				"puppet:///modules/site/tmux/${::fqdn}/${name}",
+				"puppet:///modules/site/tmux/${name}",
+				"puppet:///modules/site_tmux/${::fqdn}/${name}",
+				"puppet:///modules/site_tmux/${name}",
+				"puppet:///modules/tmux/normal" ],
+			default => $source
+		},
+		owner => $owner,
+		group => $group,
+		mode => "0644',
+  	}
 }
